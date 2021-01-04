3cb1cda96ff75d65cb3cd1a4dc073047251c5273
Jonatan Eduardo <jonataneduardo777@gmail.com>
2021-01-04 18:03:49 -0300

    primeiro commit

---
+++ models/tasks.js
@@ -0,0 +1,20 @@
+//models
+
+module.exports = app => {
+
+  return {
+        findAll: (params, callback) => {
+
+                return callback([
+               
+                   {title: "FFFFFFFFFF"},
+                   {title: "GGGGGGGGGG"},
+
+                  ]);
+
+        }
+
+
+  };
+
+};
